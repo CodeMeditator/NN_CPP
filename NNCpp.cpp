9b738ad6d07644bf188fa61bd93f6a0822ed0ce1
CodeMeditator <82141752+CodeMeditator@users.noreply.github.com>
2024-10-31 11:01:17 +0800

    Update NNCpp.cpp comments

--- NNCpp.cpp
+++ NNCpp.cpp
@@ -18,7 +18,6 @@
 using namespace Eigen;
 using namespace std;
 
-
 MatrixXf Leak_ReLu(MatrixXf, float);
 
 float MSE(MatrixXf, MatrixXf);
@@ -32,25 +31,25 @@ public:
     float BackWard();
 
 private:
-    //神经网络的输入值、输出的真实值、学习率等较为固定的值
+    // 神经网络的输入值、输出的真实值、学习率等较为固定的值
     MatrixXf input;
     MatrixXf y_true;
     float alph;
-    //神经网络待学习参数
+    // 神经网络待学习参数
     MatrixXf W1_T;
     MatrixXf B1;
     MatrixXf W2_T;
     MatrixXf B2;
     MatrixXf W3_T;
     MatrixXf B3;
-    //神经网络中间参数
+    // 神经网络中间参数
     MatrixXf Z1;
     MatrixXf A1;
     MatrixXf Z2;
     MatrixXf A2;
     MatrixXf Z3;
     MatrixXf A3;  // ==out
-    //反向传播所用参数
+    // 反向传播所用参数
     MatrixXf dW3;
     MatrixXf dB3;
     MatrixXf dW2;
@@ -63,13 +62,13 @@ private:
     MatrixXf dJ_dA3;
     MatrixXf dA3_dZ3;
     MatrixXf dZ3_dW3;
-    //对Layer2所需参数
+    // 对Layer2所需参数
     // j/w2 = j/a3 * a3/z3 * z3/a2 * a2/z2 * z2/w2
     // j/b2 = j/a3 * a3/z3 * z3/a2 * a2/z2
     MatrixXf dZ3_dA2;
     MatrixXf dA2_dZ2;
     MatrixXf dZ2_dW2;
-    //对Layer1所需参数
+    // 对Layer1所需参数
     // j/w1 = j/a3 * a3/z3 * z3/a2 * a2/z2 * z2/a1 * a1/z1 * z1/w1
     // j/b1 = j/a3 * a3/z3 * z3/a2 * a2/z2 * z2/a1 * a1/z1
     MatrixXf dZ2_dA1;
@@ -77,7 +76,7 @@ private:
     MatrixXf dZ1_W1;
 };
 
-NN::NN(MatrixXf input, MatrixXf y_true, float alph)//初始化权值
+NN::NN(MatrixXf input, MatrixXf y_true, float alph)  // 初始化权值
 {
     this->input = input;
     this->y_true = y_true;
@@ -101,10 +100,9 @@ MatrixXf NN::ForWard() {
 }
 
 float NN::BackWard() {
-    int rows_temp, cols_temp;//临时行列变量
+    int rows_temp, cols_temp;  // 临时行列变量
 
-
-    //Abount Layer3 work start!!
+    // Abount Layer3 work start!!
     this->dJ_dA3 = 2 * (this->A3 - this->y_true);
     this->dA3_dZ3 = MatrixXf::Ones(this->Z3.rows(), this->Z3.cols());
     for (rows_temp = 0; rows_temp < this->Z3.rows(); ++rows_temp) {
@@ -115,9 +113,9 @@ float NN::BackWard() {
     this->dZ3_dW3 = this->A2.transpose();
     this->dW3 = this->dJ_dA3.cwiseProduct(this->dA3_dZ3) * this->dZ3_dW3 / DataNum;
     this->dB3 = this->dJ_dA3.cwiseProduct(this->dA3_dZ3) / DataNum;
-    //Abount Layer3 work end!!
+    // Abount Layer3 work end!!
 
-    //Abount Layer2 work start!!
+    // Abount Layer2 work start!!
     this->dZ3_dA2 = this->W3_T.transpose();
     this->dA2_dZ2 = MatrixXf::Ones(this->Z2.rows(), this->Z2.cols());
     for (rows_temp = 0; rows_temp < this->Z2.rows(); ++rows_temp) {
@@ -130,9 +128,9 @@ float NN::BackWard() {
     this->dW2 = this->dA2_dZ2.cwiseProduct(this->dZ3_dA2 * this->dJ_dA3.cwiseProduct(this->dA3_dZ3)) * this->dZ2_dW2 /
                 DataNum;
     this->dB2 = this->dA2_dZ2.cwiseProduct(this->dZ3_dA2 * this->dJ_dA3.cwiseProduct(this->dA3_dZ3)) / DataNum;
-    //Abount Layer2 work end!!
+    // Abount Layer2 work end!!
 
-    //Abount Layer1 work start!!
+    // Abount Layer1 work start!!
     this->dZ2_dA1 = this->W2_T.transpose();
     this->dA1_Z1 = MatrixXf::Ones(this->Z1.rows(), this->Z1.cols());
     for (rows_temp = 0; rows_temp < this->Z1.rows(); ++rows_temp) {
@@ -148,9 +146,9 @@ float NN::BackWard() {
             this->dZ2_dA1 * this->dA2_dZ2.cwiseProduct(this->dZ3_dA2 * this->dJ_dA3.cwiseProduct(this->dA3_dZ3))) /
                 DataNum;
 
-    //Abount Layer1 work end!!
+    // Abount Layer1 work end!!
 
-    //调整学习参数
+    // 调整学习参数
     this->W3_T = this->W3_T - this->alph * this->dW3;
     this->W2_T = this->W2_T - this->alph * this->dW2;
     this->W1_T = this->W1_T - this->alph * this->dW1;
@@ -164,12 +162,12 @@ MatrixXf Leak_ReLu(MatrixXf Z, float a) {
     int Z_rows = Z.rows();
     int Z_cols = Z.cols();
     int i, j;
-    //cout << Z_rows <<" " <<Z_cols << endl;
+    // cout << Z_rows <<" " <<Z_cols << endl;
     MatrixXf A(Z_rows, Z_cols);
-    //cout << Z_rows << " " << Z_cols << endl;
+    // cout << Z_rows << " " << Z_cols << endl;
     for (i = 0; i < Z_rows; ++i) {
         for (j = 0; j < Z_cols; ++j) {
-            //cout << "i=" << i << "," << "j=" << j << endl;
+            // cout << "i=" << i << "," << "j=" << j << endl;
             A(i, j) = Z(i, j) >= 0 ? Z(i, j) : a * Z(i, j);
         }
     }
@@ -177,7 +175,7 @@ MatrixXf Leak_ReLu(MatrixXf Z, float a) {
     return A;
 }
 
-float MSE(MatrixXf y_true, MatrixXf y_pred)//损失函数
+float MSE(MatrixXf y_true, MatrixXf y_pred)  // 损失函数
 {
     MatrixXf true_pred = y_true - y_pred;
     return true_pred.array().square().sum() / true_pred.cols();
@@ -185,8 +183,8 @@ float MSE(MatrixXf y_true, MatrixXf y_pred)//损失函数
 
 int main() {
     MatrixXf input_data, y_true, y_pred;
-    int rows_temp, cols_temp;//行列数临时变量
-    int epch;//训练轮数临时变量
+    int rows_temp, cols_temp;  // 行列数临时变量
+    int epch;  // 训练轮数临时变量
     int count_right = 0, count_error = 0;
     input_data = MatrixXf::Random(InputShape, DataNum);
     y_true = MatrixXf::Zero(Layer3_OutShape, DataNum);
